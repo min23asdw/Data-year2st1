87ba44b3c672a52a2319c4bb21352f72657d6200
min23asdw <min23asdw@outlook.com>
2021-09-07 19:00:33 +0700

    Hw6 Q4

--- hw6/Tree.java
+++ hw6/Tree.java
@@ -158,220 +158,271 @@ public class Tree extends BTreePrinter { // Fix this
 
         // Use while loop to traverse from root to the closest leaf
 
-        return closest;}
+        return closest;
       }
-      ////////////////////////////       Q2    ////////////////////////////
+    }
+    ////////////////////////////       Q2    ////////////////////////////
 
 
 
 
 
-      // Implement this function using the findClosestLeaf technique
-      // Do not implement the recursive function
-      public void insert(int key) {
-        // Implement insert() using the non-recursive version
-        // This function should call findClosestLeaf()
-        if(root!=null){
-          Node n = findClosestLeaf(key);
-          if(n.key!= key){
-            if(key > n.key){
-              n.right = new Node(key);
-              n.right.parent = n;
-            }
-
-            else {
-              n.left = new Node(key);
-              n.left.parent = n;
-            }
-          }
-          else
-          {
-            return;
+    // Implement this function using the findClosestLeaf technique
+    // Do not implement the recursive function
+    public void insert(int key) {
+      // Implement insert() using the non-recursive version
+      // This function should call findClosestLeaf()
+      if(root!=null){
+        Node n = findClosestLeaf(key);
+        if(n.key!= key){
+          if(key > n.key){
+            n.right = new Node(key);
+            n.right.parent = n;
           }
 
-        }else{
-          root = new Node(key);
+          else {
+            n.left = new Node(key);
+            n.left.parent = n;
+          }
         }
-      }
-
-      public void printPreOrderDFT(){
-        System.out.print("PreOrder DFT node sequence [ ");
-        if(root != null){
-          printPreOrderDFT(root);
-        }
-        System.out.println("]");
-      }
-
-      public static void printPreOrderDFT(Node node){
-        // this function should be recursive
-        if (node!=null){
-
-          System.out.print(node.key+" ");
-          printPreOrderDFT(node.left);
-          printPreOrderDFT(node.right);
-        }
-        else {return;}
-
-      }
-
-      public void printInOrderDFT(){
-        System.out.print("InOrder DFT node sequence [ ");
-        if(root!=null){
-          printInOrderDFT(root);
-        }
-        // Call the recursive version
-        System.out.println("]");
-      }
-
-      public static void printInOrderDFT(Node node){
-        if (node!=null){
-          printInOrderDFT(node.left);
-          System.out.print(node.key+" ");
-          printInOrderDFT(node.right);
-
-        }
-        else {
+        else
+        {
           return;
         }
-        // this function should be recursive
+
+      }else{
+        root = new Node(key);
+      }
+    }
+
+    public void printPreOrderDFT(){
+      System.out.print("PreOrder DFT node sequence [ ");
+      if(root != null){
+        printPreOrderDFT(root);
+      }
+      System.out.println("]");
+    }
+
+    public static void printPreOrderDFT(Node node){
+      // this function should be recursive
+      if (node!=null){
+
+        System.out.print(node.key+" ");
+        printPreOrderDFT(node.left);
+        printPreOrderDFT(node.right);
+      }
+      else {return;}
+
+    }
+
+    public void printInOrderDFT(){
+      System.out.print("InOrder DFT node sequence [ ");
+      if(root!=null){
+        printInOrderDFT(root);
+      }
+      // Call the recursive version
+      System.out.println("]");
+    }
+
+    public static void printInOrderDFT(Node node){
+      if (node!=null){
+        printInOrderDFT(node.left);
+        System.out.print(node.key+" ");
+        printInOrderDFT(node.right);
+
+      }
+      else {
+        return;
+      }
+      // this function should be recursive
+    }
+
+    public void printPostOrderDFT(){
+      System.out.print("PostOrder DFT node sequence [ ");
+      // Call the recursive version
+      if (root!=null){
+        printPostOrderDFT(root);
+      }
+      System.out.println("]");
+    }
+
+    public static void printPostOrderDFT(Node node){
+      // this function should be recursive
+      if (node!=null){
+        printPostOrderDFT(node.left);
+        printPostOrderDFT(node.right);
+        System.out.print(node.key+" ");
+      }
+      else{return;}
+    }
+    ////////////////////////////       Q3    ////////////////////////////
+
+    public int height(){ // Tree height
+      // Hint: call the static function
+      if(root!=null){
+        return height(root);
+
+      }
+      else{return -1;}
+    }
+    public static int height(Node node){
+      // Use recursion to implement this function
+      // height = length(path{node->deepest child})
+      if(node!=null){
+        return 1 + Math.max(height(node.left), height(node.right));
+      }else{
+        return -1;
       }
 
-      public void printPostOrderDFT(){
-        System.out.print("PostOrder DFT node sequence [ ");
-        // Call the recursive version
-        if (root!=null){
-          printPostOrderDFT(root);
-        }
-        System.out.println("]");
+    }
+
+    public int size(){ // Tree size
+      // Hint: call the static function
+      if(root!=null){
+        return size(root);
+      }
+      else{
+        return 0;
+      }
+    }
+
+    public static int size(Node node){
+      // Use recursion to implement this function
+      // size = #children + 1(itself)
+      if(node!=null){
+
+        return 1 + size(node.left) + size(node.right);
+
+      }
+      else{
+        return 0;
+      }
+    }
+
+    public int depth(){ // Tree depth
+      // Hint: call the static function
+      if(root!=null){
+        return height(root);
+      }
+      else {return -1;}
+    }
+
+    public static int depth(Node root, Node node){
+      // Use recursion to implement this function
+      // Similar to height() but start from node, go up to root
+      // depth = length(path{node->root})
+      if(node!=root){
+        return 1 +  depth (root,node.parent) ;
+
+      }
+      else {
+        return 0;
       }
 
-      public static void printPostOrderDFT(Node node){
-        // this function should be recursive
-        if (node!=null){
-          printPostOrderDFT(node.left);
-          printPostOrderDFT(node.right);
-          System.out.print(node.key+" ");
-        }
-        else{return;}
-      }
-      ////////////////////////////       Q3    ////////////////////////////
+    }
 
-      public int height(){ // Tree height
-        // Hint: call the static function
-        if(root!=null){
-          return height(root);
 
-        }
-        else{return -1;}
+    public Node findKthSmallest(int k){
+      if(root!=null){
+
+        return findKthSmallest(root,k);
       }
-      public static int height(Node node){
-        // Use recursion to implement this function
-        // height = length(path{node->deepest child})
-        if(node!=null){
-          return 1 + Math.max(height(node.left), height(node.right));
+      else {
+        return null;
+      }
+    }
+
+    public static Node findKthSmallest(Node node, int k){
+      // this function should be recursive
+      if(node!=null){
+        int l = size(node.left);
+        if(k==l+1){
+          return node;
+        }else if(k<l+1){
+          return findKthSmallest(node.left,k);
         }else{
-          return -1;
-        }
-
-      }
-
-      public int size(){ // Tree size
-        // Hint: call the static function
-        if(root!=null){
-          return size(root);
-        }
-        else{
-          return 0;
+          return findKthSmallest(node.right,k-l-1);
         }
       }
-
-      public static int size(Node node){
-        // Use recursion to implement this function
-        // size = #children + 1(itself)
-        if(node!=null){
-
-            return 1 + size(node.left) + size(node.right);
-
-        }
-        else{
-          return 0;
-        }
-      }
-
-      public int depth(){ // Tree depth
-        // Hint: call the static function
-        if(root!=null){
-          return height(root);
-        }
-        else {return -1;}
-      }
-
-      public static int depth(Node root, Node node){
-        // Use recursion to implement this function
-        // Similar to height() but start from node, go up to root
-        // depth = length(path{node->root})
-        if(node!=root){
-          return 1 +  depth (root,node.parent) ;
-
-        }
-          else {
-            return 0;
-          }
-
-      }
-
-
-      public Node findKthSmallest(int k){
-        if(root!=null){
-
-          return findKthSmallest(root,k);
-        }
-        else {
-          return null;
-          }
-      }
-
-      public static Node findKthSmallest(Node node, int k){
-        // this function should be recursive
-        if(node!=null){
-          int l = size(node.left);
-          if(k==l+1){
-            return node;
-          }else if(k<l+1){
-            return findKthSmallest(node.left,k);
-          }else{
-            return findKthSmallest(node.right,k-l-1);
-          }
-        }
-          else {
-            return null;
-          }
-
-      }
-
-////////////////////////////       Q4    ////////////////////////////
-
-      public static Node findNext(Node node){
-        //this function should call other functions
+      else {
         return null;
       }
 
-      public static Node leftDescendant(Node node){// Case 1 (findMin)
-        // this function should be recursive
-        return null;
-      }
+    }
 
-      public static Node rightAncestor(Node node) {// Case 1 (first right parent)
-        // this function should be recursive
-        return null;
-      }
+    ////////////////////////////       Q4    ////////////////////////////
 
-      public List rangeSearch(int x, int y){
-        // This function utilizes findCloest() and findNext()
-        // Use List list append(node) to add node to the list
-        // List is the static Array
-        return new List(100);
-      }
+    public static Node findNext(Node node){
+       //this function should call other functions
+       if(node!=null){
+         if(node.right!=null){
+           return leftDescendant(node.right);
+         }
+         else{
+
+           return rightAncestor(node);
+
+         }
+       }
+       else {
+         return null;
+       }
+     }
+
+     public static Node leftDescendant(Node node){// Case 1 (findMin)
+       // this function should be recursive
+       if(node.left!=null){
+         return leftDescendant(node.left);
+       }
+       else{
+         return node;}
+     }
+
+     public static Node rightAncestor(Node node) {// Case 1 (first right parent)
+       // this function should be recursive
+
+
+       if(node.parent!=null){
+       if(node.key>node.parent.key){
+         return rightAncestor(node.parent);
+       }
+       else {return node.parent;}
+     }else{return node;}
+     }
+
+     public List rangeSearch(int x, int y){
+       // This function utilizes findCloest() and findNext()
+       // Use List list append(node) to add node to the list
+       // List is the static Array
+       List list = new List(y-x);
+       Node current = findClosest(x);
+       Node end = findClosest(y);
+
+     //  list.append(current);
+     // System.out.print("end 1  =  " + end.key+"    ");
+         while(current.key<=end.key)
+         {
+
+          // System.out.print("current 2=  " + current.key+"    ");
+           if(current.key<=y){
+           list.append(current);
+           }
+
+           if(current.key>=end.key){break;}
+
+           current = findNext(current);
+
+
+
+         //  System.out.print("current 3=  " + current.key);
+
+         }
+         
+
+       return list;
+     }
+
+      ////////////////////////////       Q5    ////////////////////////////
 
 
       // This function is for deleting the root node
