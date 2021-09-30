6c343f5841ab59ba5d379d7f4cddbc12ea6b5158
min23asdw <min23asdw@outlook.com>
2021-09-30 14:23:32 +0700

    Q9

--- hw7/SplayTree.java
+++ hw7/SplayTree.java
@@ -14,67 +14,219 @@ public class SplayTree extends BTreePrinter{
     // Case 2-3: x.parent == root (sig from left, zig from right)
     // Case 4-5: x.parent != root (sig from left, zig from right)
     public void zig(Node x){
+  
+        if(x!=null){
         Node y = x.parent;
         if (y == null){
             System.out.println("Cannot perform Zig operation on the root node");
-        }else if (y == root){ // If the node is a child of the root
-            if (x.key < y.key){// Zig from left
-            
-            }   // Complete the rest of the cases yourself
+        }
+        else if (y == root){ // If the node is a child of the root
+            if (x.key < y.key){
+                root = x;
+                x.parent = null;
+                if(x.right!=null){
+                    y.left =x.right;
+                    x.right.parent = y;
+                }else{
+                    y.left = null;
+                }
+                x.right = y;
+                y.parent =x;
+            }
+            else {
+                root = x;
+                x.parent =null;
+                if(x.left!=null){
+                    y.right = x.left;
+                    x.left.parent = y;
+                }else{
+                    y.right = null;
+                }
+                x.left = y;
+                y.parent = x;
+            }
+        }
+
+
+
+        else if (y != root) {
+            Node w = y.parent;
+            if(w.key > y.key){ // y เป็น left subtree ของ w
+                w.left = x;
+              }
+              else{ // y เป็น right subtree ของ w
+                w.right = x;
+              }
+    
+              x.parent = w;
+
+            if (x.key < y.key){
+ 
+                if(x.right!=null){
+                    y.left =x.right;
+                    x.right.parent = y;
+                }else{
+                    y.left = null;
+                }
+
+                x.right = y;
+                y.parent =x;
+
+            }
+            else {
+                if(x.left!=null){
+                    y.right = x.left;
+                    x.left.parent = y;
+                }else{
+                    y.right = null;
+                }
+                x.left = y;
+                y.parent = x;
+            }
+
         }
     }
+        
+    }
+    
+      
     
     // zigzig() function will move up node x two levels along the outer path
     // Pls call zig() to perform zigzig()
     public void zigzig(Node x){
-        // Do something    
+        zig(x.parent);
+        zig(x);
     }
     
     
     // zigzag() function will move up node x two levels along the inner path
     // Pls call zig() to perform zigzag()
     public void zigzag(Node x){
-        // Do something 
+        zig(x);
+        zig(x);
     }
     
     // This function will interatively splay (move up) node x all the way to the root
     public void splay(Node x){
-        while (x !=null && x != root){
+        while (x != null && x != root){ // ลากขึ้นไปจนถึง root
             Node y = x.parent;
+            // y is root just zig x
             if (y == root){
-                // Do something and break
+                zig(x);
                 break;
             }
-        }   // Complete the rest of the cases yourself
+            else {
+                Node w = y.parent;   
+                // แบบวงนอก
+                if (w.left != null && w.left.left == x || w.right != null && w.right.right == x) {
+                    zigzig(x);
+                }
+                // วงใน
+                else if (w.left != null && w.left.right == x || w.right != null && w.right.left == x) {
+                    zigzag(x);
+                }
+            }
+        }  
     }
     
-    // Modify this function to have the splaying feature
-    // This can be done by calling the splay() function
     public void insert(int key) {
-        // Pls copy code from the previous problem
-        // and add the splaying feature somewhere
-    }
-    
-    // Modify this function to have the splaying feature (if withSplay is true)
-    // This can be done by calling the splay() function
-    public Node find(int search_key, boolean withSplay){
-        // Pls copy code from the previous problem
-        // and add the splaying feature somewhere (do not forget the withSplay check)
-        return null; // Fix this
-    }
-    
-    // This delete() is different than what you learned in BSTree and AVLTree before
-    // Use the algorithm learned in the class to implement this function
-    public void delete(int key) {
+        if(root!=null){
+            
+            Node curr = root;
+            while(curr!=null){
 
+                  if(key==curr.key){
+                    System.out.println("Duplicated key:" + key);
+                    }else if(key<curr.key){
+                    
+                        if(curr.left==null){
+ 
+                               break;
+                         }
+                        curr = curr.left;
+
+                     }else if(key>curr.key){
+                      if(curr.right==null){
+
+                         break;
+
+                     }
+                        curr = curr.right;
+
+                  }
+                }
+
+                  Node temp = new Node(key);
+                  if(key<curr.key){
+                    curr.left = temp;
+                    temp.parent = curr;
+                  }else{
+                    curr.right = temp;
+                    temp.parent = curr;
+                  }
+                  splay(temp);
+            
+        }else{
+            root = new Node(key);
+        }
+    }
+    
+    // Have the splaying feature (if withSplay is true)
+    public Node find(int search_key, boolean withSplay){
+        Node curr = root;
+        // loop through tree and compare key till we find the node
+        while (curr != null) {
+
+            if (curr.key == search_key) {
+                if (withSplay){ //if true ลากขึ้น root
+                splay(curr); 
+                }
+                return curr;
+            }
+            else if (search_key < curr.key) {
+                curr = curr.left; 
+            }
+            else if (search_key > curr.key) {
+                curr = curr.right;
+            }
+        }
+
+
+
+
+        return curr;
+    }
+    
+    public void delete(int key) {
+        if (root.left == null && root.right == null && key == root.key) {
+            root = null;
+            return;
+        }
+
+        
+        splay(find(key, true));
+        //fin min rightsubtree
+        Node min = root.right;
+        while (min != null && min.left != null) {
+            min = min.left;
+        }
+        splay(min);
+        root = min;
+
+        root.left = root.left.left;
+        // reattach left subtree to new root
+        if (root.left != null) root.left.parent = root;
     }
     
     // This function does not have the splaying feature
     public Node findMin() {
-        // Pls copy code from the previous problem
-        // Do not add the splaying feature in this function
-        return null; // Fix this
+        Node curr = root;
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
     }
+    
     
     // This is another version of height() called iterative method to find BST height
     // This function is complete, no need to edit
